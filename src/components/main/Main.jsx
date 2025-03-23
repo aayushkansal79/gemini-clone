c7b17488439ad50a9283dcf3a6885e7e05824921
Aayush Kansal <aayushkansal79@gmail.com>
2025-03-23 17:19:56 +0530

    UI Complete

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -6,31 +6,44 @@ const Main = () => {
   return (
     <div className='main'>
       <div className='nav'>
-        <p>Gemini</p>
+        <p>Sasta Gemini</p>
         <img src={assets.user_icon2} alt="" />
       </div>
       <div className="main-container">
         <div className="greet">
-            <p><span>Hello, Chintu</span></p>
-            <p>How can I help you today?</p>
+            <p><span>Haan Bhai, Chintu</span></p>
+            <p>Aaj kya dikkat hai tujhe?</p>
         </div>
         <div className="cards">
             <div className="card">
-                <p>Mummy kesi h !?</p>
+                <p>Or mummy kesi hai ?</p>
                 <img src={assets.compass_icon} alt="" />
             </div>
             <div className="card">
-                <p>Would you rather sleep with.... !?</p>
+                <p>Would you rather sleep with..!?</p>
                 <img src={assets.bulb_icon} alt="" />
             </div>
             <div className="card">
-                <p>How to crack IIT ..</p>
+                <p>How to crack IIT ?</p>
                 <img src={assets.message_icon} alt="" />
             </div>
             <div className="card">
                 <p>What is the currency of South Africa ?</p>
                 <img src={assets.code_icon} alt="" />
             </div>
+        </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder='Puchh, batata hu ..' />
+            <div>
+              <img src={assets.gallery_icon} alt="" srcset="" />
+              <img src={assets.mic_icon} alt="" srcset="" />
+              <img src={assets.send_icon} alt="" srcset="" />
+            </div>
+          </div>
+          <p className='bottom-info'>
+            Pehle log khet me kaam kiya karte the, fir Gemini chalane vale aagye
+          </p>
         </div>
       </div>
     </div>
