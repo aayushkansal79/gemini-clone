12dee8a587fbb71e39de16fa4817c738b9df1c59
Rohit2133 <rohitaggarwal157@gmail.com>
2025-03-23 20:38:31 +0530

    Integrated gemini

---
+++ src/context/context.jsx
@@ -0,0 +1,33 @@
+import { createContext, useState } from "react";
+import run from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+
+    const [input,setInput] = useState("");
+    const [recentPrompt,setRecentPrompt] = useState("");
+    const [prevPrompts,setPrevPrompts] = useState([]);
+    const [showResult,setShowResult] = useState(false);
+    const [loading,setLoading] = useState(false);
+    const [resultdata,setResultData] = useState("");
+
+
+
+
+
+
+    const onSent = async (prompt) => {
+       await run(prompt)
+    }
+    const contextValue = {
+
+    }
+    return (
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+
+}
+export default ContextProvider;
